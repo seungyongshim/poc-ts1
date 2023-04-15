f62b427c86a4b186de860418755c7fcf9abd80d7
SeungYongShim <syshim@me.com>
2023-04-16 08:22:32 +0900

    .

---
+++ module.ts
@@ -0,0 +1,5 @@
+function addNumber(x : number, y: number) {
+    return x + y;
+}
+
+console.log(addNumber(1, 2));
---
+++ out/module.js
@@ -0,0 +1,5 @@
+function addNumber(x, y) {
+    return x + y;
+}
+console.log(addNumber(1, 2));
+//# sourceMappingURL=module.js.map
